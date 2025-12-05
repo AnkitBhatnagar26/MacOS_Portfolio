eedaf2bb2d45247c347e6d74de32f0471bcb25c5
Ankit Bhatnagar <ankitb2k14@gmail.com>
2025-12-05 21:14:10 +0530

    push photos feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
-import { Dock, Image, Navbar, Text, Welcome } from "#components";
+import { Dock, Home, Image, Navbar, Text, Welcome } from "#components";
 import { Contact, Finder, Resume, Safari, Terminal } from "#windows";
+import Photos from "#windows/Photos";
 
 gsap.registerPlugin(Draggable);
 
@@ -20,6 +21,8 @@ const App = () => {
       <Text />
       <Image />
       <Contact />
+      <Home />
+      <Photos />
     </div>
   );
 };
--- src/windows/index.ts
+++ src/windows/index.ts
@@ -3,5 +3,6 @@ import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
 import Contact from "./Contact";
+import Photos from "./Photos";
 
-export { Terminal, Safari, Resume, Finder, Contact };
+export { Terminal, Safari, Resume, Finder, Contact, Photos };
